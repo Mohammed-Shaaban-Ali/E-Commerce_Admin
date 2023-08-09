dc544e75b6a25e52ce8c9cefcb0deead36be6282
Mohammed Shaaban <ms7500746@gnail.com>
2023-08-09 11:46:50 +0300

    Forget Password

--- src/pages/Forms/ForgetPassword.jsx
+++ src/pages/Forms/ForgetPassword.jsx
@@ -1,7 +1,36 @@
+import CustomInput from "../../components/CustomInput";
 import "./Forms.css";
 
 const ForgetPassword = () => {
-  return <div>ForgetPassword</div>;
+  return (
+    <div
+      className="py-5 "
+      style={{ background: "#ffd333", minHeight: "100vh" }}
+    >
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <div className="m-auto my-5 w-25 bg-white rounded-3 p-4">
+        <h3 className="text-center">Forget Password</h3>
+        <p className="text-center" style={{ color: "gray" }}>
+          Enter ypur register email to get reset password mail.
+        </p>
+        <form>
+          <CustomInput type="email" id="email" label="Enter Address" />
+
+          <button
+            className="border-0 px-3 py-2 text-white fw-bold w-100"
+            style={{ background: "#ffd333" }}
+            type="submit"
+          >
+            Send Link
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default ForgetPassword;
