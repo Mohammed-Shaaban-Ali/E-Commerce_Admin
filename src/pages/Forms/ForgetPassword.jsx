debb050bdd2d56b977aabd83e3d2364ae0abc889
Mohammed Shaaban <ms7500746@gnail.com>
2023-08-09 16:37:31 +0300

    hamdle main layout

--- src/pages/Forms/ForgetPassword.jsx
+++ src/pages/Forms/ForgetPassword.jsx
@@ -3,17 +3,14 @@ import "./Forms.css";
 
 const ForgetPassword = () => {
   return (
-    <div
-      className="py-5 "
-      style={{ background: "#ffd333", minHeight: "100vh" }}
-    >
+    <div className="py-5 " style={{ background: "gray", minHeight: "100vh" }}>
       <br />
       <br />
       <br />
       <br />
       <br />
       <div className="m-auto my-5 w-25 bg-white rounded-3 p-4">
-        <h3 className="text-center">Forget Password</h3>
+        <h3 className="text-center fw-bold ">Forget Password</h3>
         <p className="text-center" style={{ color: "gray" }}>
           Enter your email to get reset password mail.
         </p>
@@ -22,7 +19,7 @@ const ForgetPassword = () => {
 
           <button
             className="border-0 px-3 py-2 text-white fw-bold w-100"
-            style={{ background: "#ffd333" }}
+            style={{ background: "gray" }}
             type="submit"
           >
             Send Link
--- src/pages/Forms/ResetPassword.jsx
+++ src/pages/Forms/ResetPassword.jsx
@@ -3,17 +3,14 @@ import "./Forms.css";
 
 const ResetPassword = () => {
   return (
-    <div
-      className="py-5 "
-      style={{ background: "#ffd333", minHeight: "100vh" }}
-    >
+    <div className="py-5 " style={{ background: "gray", minHeight: "100vh" }}>
       <br />
       <br />
       <br />
       <br />
       <br />
       <div className="m-auto my-5 w-25 bg-white rounded-3 p-4">
-        <h3 className="text-center">Reset Password</h3>
+        <h3 className="text-center  fw-bold">Reset Password</h3>
         <p className="text-center" style={{ color: "gray" }}>
           Enter Your New Password
         </p>
@@ -21,8 +18,8 @@ const ResetPassword = () => {
           <CustomInput type="password" id="password" label="New Password" />
           <CustomInput type="password" id="password" label="Confirm password" />
           <button
-            className="border-0 px-3 py-2 text-white fw-bold w-100"
-            style={{ background: "#ffd333" }}
+            className=" border-0 px-3 py-2 text-white fw-bold w-100"
+            style={{ background: "gray" }}
             type="submit"
           >
             Reset Password
