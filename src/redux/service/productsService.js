24401c0becaffff217fa33b90b3eeae79d0174f1
Mohammed Shaaban <ms7500746@gnail.com>
2023-08-18 02:30:07 +0300

    add product

--- src/redux/service/productsService.js
+++ src/redux/service/productsService.js
@@ -1,12 +1,17 @@
 import request from "../../utils/request";
+import { ConfigToken } from "../../utils/validateToken";
 
 const getProducts = async () => {
   const { data } = await request.get("/api/product/");
-
+  return data;
+};
+const addProduct = async (product) => {
+  const { data } = await request.post("/api/product/", product, ConfigToken);
   return data;
 };
 
 const productsService = {
   getProducts,
+  addProduct,
 };
 export default productsService;
