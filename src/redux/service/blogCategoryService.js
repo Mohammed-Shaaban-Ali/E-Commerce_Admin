e6125c522e614017978c31b4f635b996b376a3d6
Mohammed Shaaban <ms7500746@gnail.com>
2023-08-19 14:59:12 +0300

    Add blog category

--- src/redux/service/blogCategoryService.js
+++ src/redux/service/blogCategoryService.js
@@ -1,11 +1,20 @@
 import request from "../../utils/request";
+import { ConfigToken } from "../../utils/validateToken";
 
 const getCategory = async () => {
-  const { data } = await request.get("/api/brand-category/");
+  const { data } = await request.get("/api/blog-category/");
   return data;
 };
-
+const addBlogcategory = async (Blogcategory) => {
+  const { data } = await request.post(
+    "/api/blog-category/",
+    Blogcategory,
+    ConfigToken
+  );
+  return data;
+};
 const blogCategoryService = {
   getCategory,
+  addBlogcategory,
 };
 export default blogCategoryService;
--- src/redux/slices/blogCategorySlice.js
+++ src/redux/slices/blogCategorySlice.js
@@ -2,10 +2,20 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import blogCategoryService from "../service/blogCategoryService";
 
 export const getblogCategory = createAsyncThunk(
-  "auth/getblogCategory",
+  "Blogcategory/getblogCategory",
   async (thunkAPI) => {
     try {
       return await blogCategoryService.getCategory();
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+export const addBlogcategory = createAsyncThunk(
+  "Blogcategory/addcategory",
+  async (category, thunkAPI) => {
+    try {
+      return await blogCategoryService.addBlogcategory(category);
     } catch (error) {
       return thunkAPI.rejectWithValue(error);
     }
@@ -37,6 +47,21 @@ const blogCategorylice = createSlice({
         state.isLoading = false;
         state.isSuccess = false;
         state.blogCategory = action.error;
+      })
+
+      .addCase(addBlogcategory.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(addBlogcategory.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.createdBlogcategory = action.payload;
+      })
+      .addCase(addBlogcategory.rejected, (state, action) => {
+        state.isError = true;
+        state.isLoading = false;
+        state.isSuccess = false;
+        state.message = action.error;
       });
   },
 });
