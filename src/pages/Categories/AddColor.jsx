fa5b4e84d25b8910c4d5b9f9d81ec8f765be201b
Mohammed Shaaban <ms7500746@gnail.com>
2023-08-19 13:34:11 +0300

    Add Color

--- src/pages/Categories/AddColor.jsx
+++ src/pages/Categories/AddColor.jsx
@@ -1,16 +1,64 @@
-import React from "react";
+import { useEffect } from "react";
 import CustomInput from "../../components/CustomInput";
+import { useDispatch, useSelector } from "react-redux";
+import { object, string } from "yup";
+import { toast } from "react-toastify";
+import { useFormik } from "formik";
+import { addcolor } from "../../redux/slices/colorSlice";
+
+let userSchema = object().shape({
+  title: string().required("color Name is required"),
+});
 
 const AddColor = () => {
+  const dispatch = useDispatch();
+
+  const { isError, isLoading, isSuccess, createdcolor } = useSelector(
+    (state) => state.colors
+  );
+
+  useEffect(() => {
+    if (isSuccess && createdcolor) {
+      toast.success("Color added successfully");
+    }
+
+    if (isError) toast.error("some thing went wrong");
+  }, [isError, isLoading, isSuccess]);
+
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+    },
+    validationSchema: userSchema,
+    onSubmit: (values) => {
+      dispatch(addcolor(values));
+      formik.resetForm();
+    },
+  });
   return (
     <div>
       <div className="mt-4">
         <h3 className="mb-4">Add Color</h3>
 
         <div>
-          <form>
-            <CustomInput type="color" label="Enter Color" />
-            <button className="btn btn-success border-0 rounded-3 my-4">
+          <form onSubmit={formik.handleSubmit}>
+            <CustomInput
+              type="color"
+              label="Color Name"
+              name="title"
+              id="title"
+              onChange={formik.handleChange("title")}
+              value={formik.values.title}
+            />
+            <div className="error">
+              {formik.touched.title && formik.errors.title ? (
+                <div>{formik.errors.title}</div>
+              ) : null}
+            </div>
+            <button
+              type="submit"
+              className="btn btn-success border-0 rounded-3 my-4"
+            >
               Add Color
             </button>
           </form>
--- src/redux/slices/colorSlice.js
+++ src/redux/slices/colorSlice.js
@@ -2,10 +2,20 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import colorsService from "../service/colorService";
 
 export const getcolors = createAsyncThunk(
-  "auth/getcolors",
+  "color/getcolors",
   async (thunkAPI) => {
     try {
       return await colorsService.getcolors();
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+export const addcolor = createAsyncThunk(
+  "color/addcolor",
+  async (color, thunkAPI) => {
+    try {
+      return await colorsService.addccolor(color);
     } catch (error) {
       return thunkAPI.rejectWithValue(error);
     }
@@ -37,6 +47,21 @@ const colorslice = createSlice({
         state.isLoading = false;
         state.isSuccess = false;
         state.colors = action.error;
+      })
+
+      .addCase(addcolor.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(addcolor.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.createdcolor = action.payload;
+      })
+      .addCase(addcolor.rejected, (state, action) => {
+        state.isError = true;
+        state.isLoading = false;
+        state.isSuccess = false;
+        state.message = action.error;
       });
   },
 });
