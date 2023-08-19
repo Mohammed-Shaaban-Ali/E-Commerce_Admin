c7bdd436e9ecc4a51733564ce4ff365e5468872e
Mohammed Shaaban <ms7500746@gnail.com>
2023-08-19 13:16:49 +0300

    Add Category

--- src/redux/service/categoryService.js
+++ src/redux/service/categoryService.js
@@ -1,11 +1,21 @@
 import request from "../../utils/request";
+import { ConfigToken } from "../../utils/validateToken";
 
 const getcategory = async () => {
   const { data } = await request.get("/api/product-category/");
   return data;
 };
 
+const addcategory = async (category) => {
+  const { data } = await request.post(
+    "/api/product-category/",
+    category,
+    ConfigToken
+  );
+  return data;
+};
 const categoriesService = {
   getcategory,
+  addcategory,
 };
 export default categoriesService;
